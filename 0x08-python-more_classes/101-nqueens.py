ae87315a0ed05256508b7bd63d6ff502a845abff
ACHERKI-YOUSSEF <youssef.acherki@um5r.ac.ma>
2024-02-12 15:30:06 -0800

    m

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,32 +1,37 @@
 #!/usr/bin/python3
 import sys
 
-def is_safe(board, row, col, n):
-    for i in range(col):
-        if board[row][i] == 1:
-            return False
-    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
-        if board[i][j] == 1:
-            return False
-    for i, j in zip(range(row, n, 1), range(col, -1, -1)):
-        if board[i][j] == 1:
-            return False
-    return True
+class NQueens:
+    def __init__(self, n):
+        self.n = n
+        self.solutions = []
 
-def solve(board, col, n, solutions):
-    if col >= n:
-        solution = []
-        for i in range(n):
-            for j in range(n):
-                if board[i][j] == 1:
-                    solution.append([i, j])
-        solutions.append(solution)
-        return
-    for i in range(n):
-        if is_safe(board, i, col, n):
-            board[i][col] = 1
-            solve(board, col + 1, n, solutions)
-            board[i][col] = 0
+    def is_safe(self, board, row, col):
+        for i in range(col):
+            if board[row][i] == 1:
+                return False
+        for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
+            if board[i][j] == 1:
+                return False
+        for i, j in zip(range(row, self.n, 1), range(col, -1, -1)):
+            if board[i][j] == 1:
+                return False
+        return True
+
+    def solve(self, board, col):
+        if col >= self.n:
+            solution = []
+            for i in range(self.n):
+                for j in range(self.n):
+                    if board[i][j] == 1:
+                        solution.append([i, j])
+            self.solutions.append(solution)
+            return
+        for i in range(self.n):
+            if self.is_safe(board, i, col):
+                board[i][col] = 1
+                self.solve(board, col + 1)
+                board[i][col] = 0
 
 def main():
     if len(sys.argv) != 2:
@@ -40,10 +45,10 @@ def main():
         print("N must be at least 4")
         sys.exit(1)
 
+    queens = NQueens(n)
     board = [[0 for _ in range(n)] for _ in range(n)]
-    solutions = []
-    solve(board, 0, n, solutions)
-    for sol in solutions:
+    queens.solve(board, 0)
+    for sol in queens.solutions:
         print(sol)
 
 if __name__ == "__main__":
