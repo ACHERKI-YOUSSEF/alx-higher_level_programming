4c4104dc6a8a6b4b17cc6a5e7ded567f64b97142
ACHERKI-YOUSSEF <youssef.acherki@um5r.ac.ma>
2024-02-12 14:59:48 -0800

    m

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
+""" empty class Rectangle that defines a rectangle
+"""
+
 
 class Rectangle:
+    """ class rectangle"""
     pass
-
-
-if __name__ == "__main__":
-    my_rectangle = Rectangle()
-    print(type(my_rectangle))
-    print(my_rectangle.__dict__)
-
